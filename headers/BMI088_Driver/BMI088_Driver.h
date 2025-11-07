2978bcf7615b59ec5c9df320c50434675ed59a88
radityankn <115343392+radityankn@users.noreply.github.com>
2025-11-07 16:38:22 +0700

    Updated header file with more macros

--- headers/BMI088_Driver/BMI088_Driver.h
+++ headers/BMI088_Driver/BMI088_Driver.h
@@ -19,6 +19,8 @@ extern "C" {
 #define ACC_SENSORTIME_DATA_1       0x19
 #define ACC_SENSORTIME_DATA_2       0x1A
 #define ACC_INIT_STAT_1             0x1D
+#define SENSOR_TEMP_H               0x22
+#define SENSOR_TEMP_L               0x23
 #define ACC_FIFO_LENGTH_H           0x24
 #define ACC_FIFO_LENGTH_L           0x25
 #define ACC_FIFO_DATA               0x26
@@ -60,10 +62,51 @@ extern "C" {
 #define GYRO_FIFO_CONFIG_1          0x3E
 #define GYRO_FIFO_DATA              0x3F
 
-//Accelerometer Setting Bitmask
+//Accelerometer Status & Setting Bitmask
+#define ACC_STATUS_DRDY             0b10000000
+#define ACC_INT_STAT_1_DRDY         0b10000000
+#define ACC_FIFO_EMPTY              0x8000
+#define ACC_BWP_OSR4                0x08
+#define ACC_BWP_OSR2                0x09
+#define ACC_BWP_NORMAL              0x0A
+#define ACC_ODR_12H5                0x05
+#define ACC_ODR_25HZ                0x06
+#define ACC_ODR_50HZ                0x07
+#define ACC_ODR_100HZ               0x08
+#define ACC_ODR_200HZ               0x09
+#define ACC_ODR_400HZ               0x0A
+#define ACC_ODR_800HZ               0x0B
+#define ACC_ODR_1600HZ              0x0C
+#define ACC_RANGE_3G                0x00
+#define ACC_RANGE_6G                0x01
+#define ACC_RANGE_12G               0x02
+#define ACC_RANGE_24G               0x04
+#define FIFO_CONFIG_0_MODE_STREAM   0x00
+#define FIFO_CONFIG_0_MODE_FIFO     0x01
+#define FIFO_CONFIG_1_ACC_ENABLE    0b01000000
+#define FIFO_CONFIG_1_INT1_EN       0b00001000
+#define FIFO_CONFIG_1_INT2_EN       0b00000100
+#define INT1_IO_CONF_INT1_IN        0b00010000
+#define INT1_IO_CONF_INT1_OUT       0b00001000
+#define INT1_IO_CONF_INT1_OD        0b00000100
+#define INT1_IO_CONF_INT1_LVL       0b00000010
+#define INT2_IO_CONF_INT2_IN        0b00010000
+#define INT2_IO_CONF_INT2_OUT       0b00001000
+#define INT2_IO_CONF_INT2_OD        0b00000100
+#define INT2_IO_CONF_INT2_LVL       0b00000010
+#define INT1_INT2_MAP_DATA_INT2_DRDY    0b01000000
+#define INT1_INT2_MAP_DATA_INT2_FWM     0b00100000
+#define INT1_INT2_MAP_DATA_INT2_FFULL   0b00010000
+#define INT1_INT2_MAP_DATA_INT1_DRDY    0b00000100
+#define INT1_INT2_MAP_DATA_INT1_FWM     0b00000010
+#define INT1_INT2_MAP_DATA_INT1_FFULL   0b00000001
+#define ACC_SELF_TEST_OFF           0x00
+#define ACC_SELF_TEST_POSITIVE      0x0D
+#define ACC_SELF_TEST_NEGATIVE      0x09
 
 
-//Gyro Setting Bitmask
+//Gyro Status Code
+
 
 typedef struct {
     int acc_cs_pin;
